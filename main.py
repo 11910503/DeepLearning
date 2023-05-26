ae0a3d2ddc6092137ad66cb6414e8fbd422796d5
11910503 <64644882+11910503@users.noreply.github.com>
2023-05-26 17:12:46 +0800

    windows ok

---
+++ main.py
@@ -0,0 +1,111 @@
+import tkinter as tk
+import cv2
+from PIL import Image, ImageTk
+import predicter
+
+class CameraApp:
+    def __init__(self, window, video_source=0):
+        self.window = window
+        self.window.title("摄像头应用")
+        self.predicter=predicter.predicter('weigth/yolov5s.pt')
+
+
+        # 创建一个标签，用于显示实时图像
+        self.live_image_label = tk.Label(window)
+        self.live_image_label.pack()
+
+
+        # 创建一个新的页面，用于显示截图
+        self.captured_page = CapturedPage(window)
+
+        # 打开视频源
+        self.video_source = video_source
+        self.video_capture = cv2.VideoCapture(self.video_source)
+        if not self.video_capture.isOpened():
+            raise ValueError("无法打开视频源", video_source)
+
+        # 获取视频的宽度和高度
+        self.width = self.video_capture.get(cv2.CAP_PROP_FRAME_WIDTH)
+        self.height = self.video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+        # 更新实时图像
+        self.update_live_image()
+
+    def update_live_image(self):
+        # 从视频源读取一帧图像
+        _, frame = self.video_capture.read()
+
+        # 将图像从BGR颜色空间转换为RGB颜色空间
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        # 将图像转换为PIL图像
+        image = Image.fromarray(frame_rgb)
+        image_path = "captured_image.jpg"
+        image_fiting_path="test_image.jpg"
+        image.save(image_path)
+        #保存图像
+        self.predicter.run(image_path,image_fiting_path)
+        image2 = Image.open(image_fiting_path)
+
+        # 将图像调整到适当的大小
+        image2 = image2.resize((int(self.window.winfo_screenwidth()), int(self.window.winfo_screenheight())))
+
+        # 将PIL图像转换为Tkinter图像
+        photo = ImageTk.PhotoImage(image2)
+
+        # 在标签中显示实时图像
+        self.live_image_label.configure(image=photo)
+        self.live_image_label.image = photo
+
+        # 每隔10毫秒更新实时图像
+        self.window.after(10, self.update_live_image)
+
+    def capture_image(self):
+        # 从视频源读取当前帧图像
+        _, frame = self.video_capture.read()
+
+        # 将图像从BGR颜色空间转换为RGB颜色空间
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        # 将图像转换为PIL图像
+        image = Image.fromarray(frame_rgb)
+
+        # 保存图像
+        image_path = "captured_image.jpg"
+        image.save(image_path)
+        print(f"已截取并保存图像：{image_path}")
+
+        # 更新截图页面的图像
+        self.captured_page.update_image(image_path)
+
+class CapturedPage:
+    def __init__(self, window):
+        self.window = window
+
+        # 创建一个标签，用于显示截图
+        self.image_label = tk.Label(window)
+        self.image_label.pack()
+
+    def update_image(self, image_path):
+        # 读取保存的图像
+        image = Image.open(image_path)
+
+        # 将图像调整到适当的大小
+        image = image.resize((int(self.window.winfo_screenwidth()), int(self.window.winfo_screenheight())))
+
+        # 将PIL图像转换为Tkinter图像
+        photo = ImageTk.PhotoImage(image)
+
+        # 在标签中显示截图
+        self.image_label.configure(image=photo)
+        self.image_label.image = photo
+
+if __name__ == "__main__":
+    # 创建一个Tkinter窗口
+    root = tk.Tk()
+
+    # 创建一个摄像头应用实例
+    app = CameraApp(root)
+
+    # 运行Tkinter事件循环
+    root.mainloop()
---
+++ predicter.py
@@ -0,0 +1,89 @@
+import argparse
+import os
+import platform
+import sys
+from pathlib import Path
+from PIL import Image
+
+import torch
+from models.common import DetectMultiBackend
+from utils.dataloaders import IMG_FORMATS, VID_FORMATS, LoadImages, LoadScreenshots, LoadStreams
+from utils.general import (LOGGER, Profile, check_file, check_img_size, check_imshow, check_requirements, colorstr, cv2,
+                           increment_path, non_max_suppression, print_args, scale_boxes, strip_optimizer, xyxy2xywh)
+from utils.plots import Annotator, colors, save_one_box
+from utils.torch_utils import select_device, smart_inference_mode
+class predicter:
+    def __init__(self,weight):
+        device = select_device('')
+        model = DetectMultiBackend(weight, device=device, dnn=False, data='data\\coco128.yaml', fp16=False)
+        stride, names, pt = model.stride, model.names, model.pt
+
+
+        self.model=model
+        self.stride=stride
+        self.names=names
+        self.pt=pt
+        self.bs=1
+        self.imgsz = check_img_size((640, 640), s=self.stride)  # check image size
+        print("model ready")
+
+    def run(self,pic,save_path):
+        dataset = LoadImages(pic, img_size=self.imgsz, stride=self.stride, auto=self.pt, vid_stride=1)
+        self.model.warmup(imgsz=(1 if self.pt or self.model.triton else self.bs, 3, *self.imgsz))  # warmup
+        seen, windows, dt = 0, [], (Profile(), Profile(), Profile())
+        for path, im, im0s, vid_cap, s in dataset:
+            with dt[0]:
+                im = torch.from_numpy(im).to(self.model.device)
+                im = im.half() if self.model.fp16 else im.float()  # uint8 to fp16/32
+                im /= 255  # 0 - 255 to 0.0 - 1.0
+                if len(im.shape) == 3:
+                    im = im[None]  # expand for batch dim
+
+            # Inference
+            with dt[1]:
+                pred = self.model(im, augment=False, visualize=False)
+
+            # NMS
+            with dt[2]:
+                pred = non_max_suppression(pred, 0.25, 0.45, None, False, max_det=1000)
+
+            # Second-stage classifier (optional)
+            # pred = utils.general.apply_classifier(pred, classifier_model, im, im0s)
+
+            # Process predictions
+            for i, det in enumerate(pred):  # per image
+                seen += 1
+
+                p, im0, frame = path, im0s.copy(), getattr(dataset, 'frame', 0)
+
+                p = Path(p)  # to Path
+                s += '%gx%g ' % im.shape[2:]  # print string
+                gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+                annotator = Annotator(im0, line_width=3, example=str(self.names))
+
+                if len(det):
+                    # Rescale boxes from img_size to im0 size
+                    det[:, :4] = scale_boxes(im.shape[2:], det[:, :4], im0.shape).round()
+
+                    # Print results
+                    for c in det[:, 5].unique():
+                        n = (det[:, 5] == c).sum()  # detections per class
+                        s += f"{n} {self.names[int(c)]}{'s' * (n > 1)}, "  # add to string
+
+                    # Write results
+                    for *xyxy, conf, cls in reversed(det):
+                        xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()
+                        line = (cls, *xywh, conf)
+                        c = int(cls)  # integer class
+                        label = f'{self.names[c]}'
+                        annotator.box_label(xyxy, label, color=colors(c, True))
+                        print(('%g ' * len(line)).rstrip() % line + '\n')
+
+
+                # Stream results
+                im0=annotator.result()
+
+                cv2.imwrite(save_path, im0)
+
+
+
