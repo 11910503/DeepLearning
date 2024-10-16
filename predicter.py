e3f493789bef4363d12589970eb4afc84cc3502c
11910503 <64644882+11910503@users.noreply.github.com>
2024-10-16 10:34:52 +0800

    'useinbama1.0'

--- predicter.py
+++ predicter.py
@@ -2,6 +2,7 @@ import argparse
 import os
 import platform
 import sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from pathlib import Path
 from PIL import Image
 import numpy as np
