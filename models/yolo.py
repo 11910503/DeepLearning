e5bbb97d350262ecc86993a83941a89e78295b73
11910503 <64644882+11910503@users.noreply.github.com>
2024-10-21 15:24:50 +0800

    'change_utils'

--- models/yolo.py
+++ models/yolo.py
@@ -23,11 +23,11 @@ if platform.system() != 'Windows':
 
 from models.common import *
 from models.experimental import *
-from utils.autoanchor import check_anchor_order
-from utils.general import LOGGER, check_version, check_yaml, make_divisible, print_args
-from utils.plots import feature_visualization
-from utils.torch_utils import (fuse_conv_and_bn, initialize_weights, model_info, profile, scale_img, select_device,
-                               time_sync)
+from yoloutils.autoanchor import check_anchor_order
+from yoloutils.general import LOGGER, check_version, check_yaml, make_divisible, print_args
+from yoloutils.plots import feature_visualization
+from yoloutils.torch_utils import (fuse_conv_and_bn, initialize_weights, model_info, profile, scale_img, select_device,
+                                   time_sync)
 
 try:
     import thop  # for FLOPs computation
--- predicter.py
+++ predicter.py
@@ -9,20 +9,18 @@ import numpy as np
 
 import torch
 from models.common import DetectMultiBackend
-from utils.augmentations import (Albumentations, augment_hsv, classify_albumentations, classify_transforms, copy_paste,
-                                 letterbox, mixup, random_perspective)
-from utils.dataloaders import IMG_FORMATS, VID_FORMATS, LoadImages, LoadScreenshots, LoadStreams
-from utils.general import (LOGGER, Profile, check_file, check_img_size, check_imshow, check_requirements, colorstr, cv2,
-                           increment_path, non_max_suppression, print_args, scale_boxes, strip_optimizer, xyxy2xywh)
-from utils.plots import Annotator, colors, save_one_box
-from utils.torch_utils import select_device, smart_inference_mode
+from yoloutils.augmentations import (Albumentations, augment_hsv, classify_albumentations, classify_transforms, copy_paste,
+                                     letterbox, mixup, random_perspective)
+from yoloutils.dataloaders import IMG_FORMATS, VID_FORMATS, LoadImages, LoadScreenshots, LoadStreams
+from yoloutils.general import (LOGGER, Profile, check_file, check_img_size, check_imshow, check_requirements, colorstr, cv2,
+                               increment_path, non_max_suppression, print_args, scale_boxes, strip_optimizer, xyxy2xywh)
+from yoloutils.plots import Annotator, colors, save_one_box
+from yoloutils.torch_utils import select_device, smart_inference_mode
 class predicter:
     def __init__(self,weight):
-        device = select_device('cpu')
+        device = select_device('')
         model = DetectMultiBackend(weight, device=device, dnn=False, data='data/coco128.yaml', fp16=False)
         stride, names, pt = model.stride, model.names, model.pt
-
-
         self.model=model
         self.stride=stride
         self.names=names
@@ -58,7 +56,7 @@ class predicter:
                 pred = non_max_suppression(pred, 0.25, 0.45, None, False, max_det=1000)
 
             # Second-stage classifier (optional)
-            # pred = utils.general.apply_classifier(pred, classifier_model, im, im0s)
+            # pred = yoloutils.general.apply_classifier(pred, classifier_model, im, im0s)
 
             # Process predictions
             for i, det in enumerate(pred):  # per image
@@ -74,7 +72,6 @@ class predicter:
                 if len(det):
                     # Rescale boxes from img_size to im0 size
                     det[:, :4] = scale_boxes(im.shape[2:], det[:, :4], im0.shape).round()
-
                     # Print results
                     for c in det[:, 5].unique():
                         n = (det[:, 5] == c).sum()  # detections per class
@@ -86,10 +83,10 @@ class predicter:
                         line = (cls, *xywh, conf)
                         c = int(cls)  # integer class
                         label = f'{self.names[c]}'
-                        if label=="person":
-                            annotator.box_label(xyxy, label, color=colors(c, True))
-                        print(('%g ' * len(line)).rstrip() % line + '\n')
 
+                        annotator.box_label(xyxy, label, color=colors(c, True))
+
+                        print(('%g ' * len(line)).rstrip() % line + '\n')
 
                 # Stream results
                 im0=annotator.result()
@@ -122,6 +119,7 @@ class predicter:
                     label = f'{self.names[c]}'
 
                     annotator.box_label(xyxy, label, color=colors(c, True))
+
             # Stream results
             im0 = annotator.result()
             return im0
@@ -137,8 +135,32 @@ class predicter:
             im = im[None]  # expand for batch dim
         pred = self.model(im, augment=False, visualize=False)
         pred = non_max_suppression(pred, 0.25, 0.45, None, False, max_det=1000)
-
         return  pred
+    def runlabel(self,pic):
+        im0s = pic.copy()
+        im = letterbox(im0s, self.imgsz, stride=self.stride, auto=True)[0]  # padded resize
+        im = im.transpose((2, 0, 1))[::-1]  # HWC to CHW, BGR to RGB
+        im = np.ascontiguousarray(im)
+        im = torch.from_numpy(im).to(self.model.device)
+        im = im.half() if self.model.fp16 else im.float()  # uint8 to fp16/32
+        im /= 255  # 0 - 255 to 0.0 - 1.0
+        if len(im.shape) == 3:
+            im = im[None]  # expand for batch dim
+        pred = self.model(im, augment=False, visualize=False)
+        pred = non_max_suppression(pred, 0.25, 0.45, None, False, max_det=1000)
+        ans=[]
+        for i, det in enumerate(pred):  # per image
+
+            gn = torch.tensor(pic.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+
+            if len(det):
+                # Rescale boxes from img_size to im0 size
+                det[:, :4] = scale_boxes(im.shape[2:], det[:, :4], pic.shape).round()
+                for *xyxy, conf, cls in reversed(det):
+                    xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()
+                    line = (cls, *xywh)
+                    ans.append(line)
+        return ans
 
 
 
